6e86f13d8e62998ce428e184dee78512d4a9bbea
Eric Seifert <seiferteric@gmail.com>
2013-02-22 19:21:49 -0800

    add yaml dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
       name = "graphit",
       version = "0.0.1",
       packages = find_packages(),
-      install_requires=['distribute', 'requests>=1.1.0'],
+      install_requires=['distribute', 'requests>=1.1.0', 'yaml'],
       author = "Eric Seifert",
       author_email = "seiferteric@gmail.com",
       description = "Graphit.io Graphing API library",

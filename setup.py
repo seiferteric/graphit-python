7483ad0503d11f641c2ecc6f01cc01352d62fccb
Eric Seifert <seiferteric@gmail.com>
2013-03-11 20:41:41 -0700

    version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
       name = "graphit.io",
-      version = "0.0.5",
+      version = "0.0.6",
       packages = find_packages(),
       install_requires=['distribute>=0.6.35', 'requests>=1.1.0', 'pyyaml'],
       author = "Eric Seifert",
